6924133a8083053b46cd3e080047d9f3a5ffb1a2
Alexander Lipatov <lap1993.12@yandex.ru>
2024-11-05 11:14:05 +0300

    commit tasks 1,2

---
+++ task_1.py
@@ -0,0 +1,88 @@
+# Реализуйте lru_cache декоратор.
+
+# Требования:
+
+#   Декоратор должен кешировать результаты вызовов функции на 
+# основе её аргументов.
+#   Если функция вызывается с теми же аргументами, что и ранее, 
+# возвращайте результат из кеша вместо повторного выполнения функции.
+#   Декоратор должно быть возможно использовать двумя способами: 
+# с указанием максимального кол-ва элементов и без.
+
+import unittest.mock
+
+
+def lru_cache(func=None, maxsize=None):
+    def decorator(func):
+        cache = dict()
+        order = list()
+
+        def wrapper(*args, **kwargs):
+            key = (args, frozenset(kwargs.items()))
+
+            # Если ключ уже есть в кэше, перемещаем его в конец списка order
+            if key in cache:
+                order.remove(key)
+                order.append(key)
+                return cache[key]
+            
+            # Вычисляем результат и добавляем его в кэш
+            result = func(*args, **kwargs)
+            cache[key] = result
+            order.append(key)
+
+            # Если cache переполнен, то удаляем самый старый элемент 
+            if maxsize is not None and len(cache) > maxsize:
+                old_key = order.pop(0)
+                del cache[old_key]
+
+            return result
+
+        return wrapper
+
+    if func:
+        return decorator(func)
+
+    return decorator
+
+
+@lru_cache
+def sum(a: int, b: int) -> int:
+    return a + b
+
+
+@lru_cache
+def sum_many(a: int, b: int, *, c: int, d: int) -> int:
+    return a + b + c + d
+
+
+@lru_cache(maxsize=3)
+def multiply(a: int, b: int) -> int:
+    return a * b
+
+
+if __name__ == '__main__':
+
+    assert sum(1, 2) == 3
+    assert sum(3, 4) == 7
+
+    assert multiply(1, 2) == 2
+    assert multiply(3, 4) == 12
+
+    assert sum_many(1, 2, c=3, d=4) == 10
+
+    mocked_func = unittest.mock.Mock()
+    mocked_func.side_effect = [1, 2, 3, 4]
+
+    decorated = lru_cache(maxsize=2)(mocked_func)
+    assert decorated(1, 2) == 1
+    assert decorated(1, 2) == 1
+    assert decorated(3, 4) == 2
+    assert decorated(3, 4) == 2
+    assert decorated(5, 6) == 3
+    assert decorated(5, 6) == 3
+    assert decorated(1, 2) == 4
+    assert mocked_func.call_count == 4
+
+
+
---
+++ task_2.py
@@ -0,0 +1,40 @@
+#   Создайте декоратор access_control, который ограничивает доступ 
+# к функции на основе переданных ролей пользователя. Декоратор должен 
+# принимать аргументы, определяющие допустимые роли (например, 
+# @access_control(roles=['admin', 'moderator'])). Требования:
+
+#   Если текущий пользователь имеет одну из допустимых ролей, 
+# функция выполняется.
+#   Если нет, выбрасывается исключение PermissionError с соответствующим 
+# сообщением.
+#   Реализуйте механизм определения текущей роли пользователя. Для целей 
+# задания можно использовать глобальную переменную или контекстный менеджер.
+
+
+def access_control(roles:list=None):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if user_role in roles:
+                return func(*args, **kwargs)
+            else:
+                raise PermissionError("Access denied")
+        return wrapper
+    return decorator
+
+
+user_role = 'admin'
+
+@access_control(roles=['admin', 'moderator'])
+def func():
+    pass
+
+func()
+
+
+user_role = 'user'
+
+@access_control(roles=['admin', 'moderator'])
+def func():
+    pass
+
+func() 
