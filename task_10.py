86963a88c4c686bf9f190820ae34d3d0abf96836
Alexander Lipatov <lap1993.12@yandex.ru>
2024-11-21 17:43:43 +0300

    fix

--- task_10.py
+++ task_10.py
@@ -4,10 +4,11 @@ from datetime import timedelta
 from redis import Redis
 
 
-redis_client = Redis('redis-18750.c92.us-east-1-3.ec2.redns.redis-cloud.com', 18750, password='ZhGSoO5yah5xlifyAP5QgN8qYylhxmi2')
+redis_client = Redis('redis-18750.c92.us-east-1-3.ec2.redns.redis-cloud.com',
+                     18750, password='ZhGSoO5yah5xlifyAP5QgN8qYylhxmi2')
 
 
-def single(max_processing_time:timedelta):
+def single(max_processing_time: timedelta):
     def decorator(func):
         def wrap(*args, **kwargs):
             lock_key = f"lock:{func.__name__}"
@@ -15,15 +16,19 @@ def single(max_processing_time:timedelta):
             lock_timeout = max_processing_time.total_seconds()
 
             if redis_client.set(lock_key, lock_id, nx=True, ex=int(lock_timeout)):
+
                 func(*args, **kwargs)
-                
+                redis_client.delete(lock_key)
+
         return wrap
     return decorator
+
+#
 
 
 @single(max_processing_time=timedelta(seconds=15))
 def process_transaction():
-    time.sleep(2)
+    time.sleep(10)
 
 
 process_transaction()
